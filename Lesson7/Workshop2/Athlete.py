ac0796d03ab58ea004d3abbd5cf8335458466156
BrandNewWish <shevaa2005@hotmail.com>
2026-01-29 14:50:09 +0100

    Delete Lesson7/Workshop2 directory

--- Lesson7/Workshop2/Athlete.py
+++
@@ -1,7 +0,0 @@
-class Athlete:
-    def __init__(self, sport, team):
-        self.sport = sport
-        self.team = team
-
-    def train(self):
-        print(f"Training in {self.sport} for team {self.team}")
